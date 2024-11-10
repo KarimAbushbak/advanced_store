b6b997d6525f83095490a2a847472d114d71aab7
KarimAbushbak <karimabushbak@gmail.com>
2024-11-10 13:23:30 +0200

    making the splash view

--- lib/features/splash/presntaion/view/splash_view.dart
+++ lib/features/splash/presntaion/view/splash_view.dart
@@ -1,12 +1,46 @@
+import 'package:advanced_store/core/resources/manager_width.dart';
+import 'package:advanced_store/features/splash/presntaion/controller/splash_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../../core/resources/manager_assets.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red,
+    return GetBuilder<SplashController>(
+      builder: (context) {
+        return Scaffold(
+          extendBodyBehindAppBar: true,
+          body: Stack(
+            children: [
+              Container(
+                width: ManagerWidth.w360, // Ensures the container spans the full width
+
+
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(ManagerAssets.splash2),
+                    alignment: Alignment.centerLeft, // Aligns the image to the left
+
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      ManagerAssets.splash1
+                    )
+                  )
+                ),
+              )
+            ],
+          ),
+        );
+      }
     );
   }
 }
